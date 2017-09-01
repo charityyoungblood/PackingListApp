16793b0784c10a8a7cf6d83b3dd91cc2c0009c1e
Charity <Youngblood.charity@gmail.com>
2017-08-31 19:23:20 -0700

    added JavaScript to packinglistapp.js for conditional - if 'male' is checked, the page should alert correct - for testing

--- packinglistapp.js
+++ packinglistapp.js
@@ -1,3 +1,8 @@
-if() {
-    
+window.onload = function() {
+    document.getElementById("male-gender").onclick = function(){
+        var genderQuestion = document.getElementById("male-gender");
+        if (genderQuestion.checked){
+            alert("CORRECT");
+        }
+     };
 }
