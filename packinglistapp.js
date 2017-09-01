b15dde48470e7eb4fcf6581c06f28d80ade49590
Charity <Youngblood.charity@gmail.com>
2017-08-31 19:47:18 -0700

    adjusted h2 titles of categories to center; moved 'before leaving' up closer to top

--- packinglistapp.js
+++ packinglistapp.js
@@ -1,8 +1,23 @@
+
+/* If user selects "male" display additional products with .male-products class
+   If user selects "female" display additional products with .female-products class
+   */
+var x = document.getElementById("pleasure-questions").required;
+
 window.onload = function() {
     document.getElementById("male-gender").onclick = function(){
-        var genderQuestion = document.getElementById("male-gender");
-        if (genderQuestion.checked){
-            alert("CORRECT");
+        var maleQuestion = document.getElementById("male-gender");
+        if (maleQuestion.checked){
+            ;
+        }
+     };
+}
+
+window.onload = function() {
+    document.getElementById("female-gender").onclick = function(){
+        var femaleQuestion = document.getElementById("female-gender");
+        if (femaleQuestion.checked){
+            ;
         }
      };
 }
