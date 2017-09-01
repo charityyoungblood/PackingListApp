d2f38d1f85506082b7712eb2c54091bb962e26ab
Charity <Youngblood.charity@gmail.com>
2017-08-31 19:56:00 -0700

    updated names of files to be included in one overall folder, added additional features I need to add to app

--- packinglistapp.js
+++ packinglistapp.js
@@ -1,6 +1,9 @@
 
-/* If user selects "male" display additional products with .male-products class
-   If user selects "female" display additional products with .female-products class
+/* Features to Add:
+
+- If user selects "male" display additional products with .male-products class
+- If user selects "female" display additional products with .female-products class
+- Once user enters location info, integrate weather API to tell if location is hot or cold - make packing recommendations based on weather conditions
    */
 var x = document.getElementById("pleasure-questions").required;
 
